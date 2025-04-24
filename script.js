ac5c55a607a565f195c28bc043cf870c1c671a70
Dereje <dereje2012k@gmail.com>
2025-04-24 15:47:50 +0300

    Create playRound function and implement scoring system

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let randNum = Math.floor(Math.random() * 3) + 1;
 
@@ -13,9 +16,39 @@ function getComputerChoice() {
 // console.log(getComputerChoice());
 
 function getHumanChoice() {
-  let humanChoice = prompt("Choose from rock, paper, or scissors");
+  // let humanChoice = prompt("Choose from rock, paper, or scissors");
 
-  return humanChoice;
+  return prompt("Choose from rock, paper, or scissors");
 }
 
 // console.log(getHumanChoice());
+
+const humanChoice = getHumanChoice().toLocaleLowerCase();
+const computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`
+      human choice: ${humanChoice},
+      computer choice: ${computerChoice}
+    `);
+
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    console.log("You Win! Rock beats Scissors. Score = " + humanScore);
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    console.log("You Win! Paper beats Rock. Score = " + humanScore);
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    console.log("You Win! Scissors beats Paper. Score = " + humanScore);
+  } else if (humanChoice === computerChoice) {
+    console.log("Tie! Refresh the page to restart the game.");
+  } else {
+    computerScore++;
+    console.log(
+      `Computer Win! ${computerChoice} beats ${humanChoice}. Score = ${computerScore}`
+    );
+  }
+}
+
+playRound(humanChoice, computerChoice);
