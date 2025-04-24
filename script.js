706c25a6f9956c694e542550d2fe09e362ba3a90
Dereje <dereje2012k@gmail.com>
2025-04-24 12:53:34 +0300

    Create a function to get computer choice rock, paper or scissors string

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+  let randNum = Math.floor(Math.random() * 3) + 1;
+
+  if (randNum === 1) {
+    return "rock";
+  } else if (randNum === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
