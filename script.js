450ae3dd7efab1e9cac5c2c7f7e518c6c34251b9
Dereje <dereje2012k@gmail.com>
2025-05-01 08:15:25 +0300

    Add buttons for user choices and update playGame function to handle button clicks

--- script.js
+++ script.js
@@ -10,17 +10,39 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  return prompt(
-    "Please enter your choice: rock, paper, or scissors"
-  ).toLowerCase();
-}
+// function getHumanChoice() {
+//   return prompt(
+//     "Please enter your choice: rock, paper, or scissors"
+//   ).toLowerCase();
+// }
 
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
 
-  function playRound(humanChoice, computerChoice, counter) {
+  const rockBtn = document.querySelector(".rock");
+  const paperBtn = document.querySelector(".paper");
+  const scissorsBtn = document.querySelector(".scissors");
+
+  rockBtn.addEventListener("click", () => {
+    const humanChoice = "rock";
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  });
+
+  paperBtn.addEventListener("click", () => {
+    const humanChoice = "paper";
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  });
+
+  scissorsBtn.addEventListener("click", () => {
+    const humanChoice = "scissors";
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  });
+
+  function playRound(humanChoice, computerChoice) {
     console.log(`
         human choice: ${humanChoice},
         computer choice: ${computerChoice}
@@ -29,50 +51,50 @@ function playGame() {
     if (humanChoice === "rock" && computerChoice === "scissors") {
       humanScore++;
       console.log(
-        `Round ${counter} result: You Win! Rock beats Scissors. Your Score = ${humanScore} and Computer Score = ${computerScore}.`
+        `Round ? result: You Won! Rock beats Scissors. Your Score = ${humanScore} and Computer Score = ${computerScore}.`
       );
     } else if (humanChoice === "paper" && computerChoice === "rock") {
       humanScore++;
       console.log(
-        `Round ${counter} result: You Win! Paper beats Rock. Your Score = ${humanScore} and Computer Score = ${computerScore}.`
+        `Round ? result: You Won! Paper beats Rock. Your Score = ${humanScore} and Computer Score = ${computerScore}.`
       );
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
       humanScore++;
       console.log(
-        `Round ${counter} result: You Win! Scissors beats Paper. Your Score = ${humanScore} and Computer Score = ${computerScore}.`
+        `Round ? result: You Won! Scissors beats Paper. Your Score = ${humanScore} and Computer Score = ${computerScore}.`
       );
     } else if (humanChoice === computerChoice) {
       console.log(
-        `Round ${counter} result: Tie!  Your Score = ${humanScore} and Computer Score = ${computerScore}.`
+        `Round ? result: Tie!  Your Score = ${humanScore} and Computer Score = ${computerScore}.`
       );
     } else {
       computerScore++;
       console.log(
-        `Round ${counter} result: Computer Win! ${computerChoice} beats ${humanChoice}. Computer's Score = ${computerScore} and Your Score = ${humanScore}.`
+        `Round ? result: Computer Won! ${computerChoice} beats ${humanChoice}. Computer's Score = ${computerScore} and Your Score = ${humanScore}.`
       );
     }
   }
 
-  for (i = 1; i <= 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+  // for (i = 1; i <= 5; i++) {
+  //   const humanChoice = getHumanChoice();
+  //   const computerChoice = getComputerChoice();
 
-    playRound(humanChoice, computerChoice, i);
-  }
+  //   playRound(humanChoice, computerChoice, i);
+  // }
 
   if (humanScore > computerScore) {
     console.log(`
 
         *******  Total Result            Score  *******
 
-                  YOU WIN!!!             ${humanScore}
+                  YOU Won!!!             ${humanScore}
       `);
   } else if (computerScore > humanScore) {
     console.log(`
 
       *******  Total Result              Score  *******
 
-                COMPUTER WIN!!!          ${computerScore}
+                COMPUTER Won!!!          ${computerScore}
     `);
   } else {
     console.log(`
